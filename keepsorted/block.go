36fcf2f0c730d3953b5cf9e23ca6dbea0421532e
Jeffrey Faer <jfaer@google.com>
2024-09-22 09:38:31 -0600

    refactor: Rework how we parse the option string.
     - Instead of checking the string for each option that exists, loop
       through the string to discover which options have been specified
     - Split it out into a parser that's arguably overkill for what we're
       currently doing, but consider this as a prefactor for https://github.com/google/keep-sorted/pull/29

--- keepsorted/block.go
+++ keepsorted/block.go
@@ -109,7 +109,8 @@ func (f *Fixer) newBlocks(lines []string, offset int, include func(start, end in
 				continue
 			}
 
-			opts, err := parseBlockOptions(start.line, f.defaultOptions)
+			commentMarker, options, _ := strings.Cut(start.line, f.startDirective)
+			opts, err := parseBlockOptions(commentMarker, options, f.defaultOptions)
 			if err != nil {
 				// TODO(b/250608236): Is there a better way to surface this error?
 				log.Err(fmt.Errorf("keep-sorted block at index %d had bad start directive: %w", start.index+offset, err)).Msg("")
