b8641a2cf200b51fb8e679e7b94061e9c94c32e2
Will Beason <willbeason@gmail.com>
2026-03-19 12:18:14 -0700

    feat: Add warning if skip_lines causes empty block (#124)
    Fixes: https://github.com/google/keep-sorted/issues/123
    
    Specifically, this triggers if skip_lines causes the block's start to be
    at or after the block's end. So fully-empty blocks with no skip_lines
    (or no-op skip_lines, such as skip_lines=0,0) are still fine.
    
    Modify tests in skip_lines golden test to show this behavior.
    
    Modify how start and end are logged in cmd.go. As-is causes the slightly
    confusing behavior that "end" is always logged before start because it
    comes first alphabetically. Now these are logged as [start,end]=[x,y].
    
    The warning for if start>=end shows both the original start/end and
    their offsets to make it more obvious how the values are arrived at.
    This helps finding the problematic lines, as otherwise you just see
    where the block start and end is, not where the directives are.
    
    ---------
    
    Co-authored-by: Jeffrey Faer <jeffrey.faer@gmail.com>

--- keepsorted/block.go
+++ keepsorted/block.go
@@ -15,6 +15,7 @@
 package keepsorted
 
 import (
+	"fmt"
 	"slices"
 	"strings"
 
@@ -116,6 +117,10 @@ func (f *Fixer) newBlocks(filename string, lines []string, offset int, include f
 			start.index += opts.startOffset()
 			endIndex += opts.endOffset()
 			if start.index >= endIndex {
+				warnings = append(warnings, finding(filename, start.index-opts.startOffset()+offset, endIndex-opts.endOffset()+offset,
+					fmt.Sprintf("block start is at or after end, possibly due to skip_lines [%d+%d,%d-%d]",
+						start.index-opts.startOffset()+offset, opts.startOffset(), endIndex-opts.endOffset()+offset, -opts.endOffset())),
+				)
 				continue
 			}
 
